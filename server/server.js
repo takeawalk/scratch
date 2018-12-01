9307d1249c9c205fadd1b58d98ac71ecd2ffe3e3
codemccodeface <yurenfrankma@gmail.com>
2018-12-01 10:58:17 -0800

    starting database branch

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const dbContoller = require('./dbcontroller');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+
+
 // app.use(express.static(path.resolve(__dirname, '../dist/')));
 
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '../index.html')));
+
+app.get('/userdata', dbContoller.getUserState, (req, res) => {
+  res.json(res.locals.state);
+});
+
 app.use(express.static(path.resolve(__dirname, '../dist/')));
 
 
