29b6f304757adff551bedb34eff277a3ffe98f6b
Dave Marquess <dave.marquess@gmail.com>
2018-12-01 14:39:46 -0800

    PART 3

--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Login from './LogIn';
+import SigUp from './SignUp';
 
 const mapStateToProps = () => ({
 
@@ -12,7 +14,8 @@ const mapDispatchToProps = dispatch => ({
 const App = props => (
   <BrowserRouter>
     <div className="container">
-      <Navigation />
+      <SignUp />
+      <LogIn />
       <Route path="/add" component={} />
       <Route path="/view" component={} />
     </div>
