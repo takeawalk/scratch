b44f56c3f99a5b6345d6702647cb2bbb85bcfb3f
codemccodeface <yurenfrankma@gmail.com>
2018-12-01 14:25:46 -0800

    build server

--- server/dbcontroller.js
+++ server/dbcontroller.js
@@ -1,8 +1,13 @@
 const pgp = require('pg-promise')();
 
-const dbController = {};
-const db = pgp('postgres://leslygmy:uYPKynsxZKpL8a98_aXmg9GEsmZ4I3Nk@elmer.db.elephantsql.com:5432/leslygmy');
+const {
+  nameGoalsAPPromise,
+  daysPromise,
+  insertGoalPromise,
+  insertNewUserPromise,
+} = require('./model');
 
+const dbController = {};
 
 dbController.findUsers = async (req, res, next) => {
   const users = await db.query('select * from users;');
@@ -10,57 +15,67 @@ dbController.findUsers = async (req, res, next) => {
   next();
 };
 
-dbController.getUserState = async (req, res, next) => {
+
+dbController.newUser = (req, res, next) => {
+  // input the new user in the users table
+  const user = req.body;
+  insertNewUserPromise(user)
+    .then(() => next())
+    .catch(err => res.send(err));
+};
+
+dbController.getUserState = (req, res, next) => {
   const { userID } = req.body;
-  const state = {};
-  
-  const userNamePromise = db.one(`select name from users where user_id=${userID};`);
-  
-  const userGoalsPromise = db.query(`select goal, value
-  from weeklygoals wg join goals g on wg.goal_id = g.goal_id
-  where user_id =${userID};`);
-  
-  
-  const accForPromise = db.one(`select u2.name, u2.user_id
-  from users u1 join users u2 on u1.accfor = u2.user_id
-  where u1.user_id = ${userID};`);
-  
-  const daysPromise = db.query(`select name, date, goal, value
-  from progress p join goals g on p.goal_id = g.goal_id join users u on p.user_id = u.user_id
-  where p.user_id = ${userID};`);
-  // userNamePromise, userGoalsPromise, accForPromise,
-  Promise.all([userNamePromise, userGoalsPromise, accForPromise, daysPromise])
+  Promise.all([nameGoalsAPPromise(userID), daysPromise(userID)])
     .then((result) => {
-      console.log(result);
-      // state.userName = result[0].name;
-      // state.goals = goalsParser(result[1]);
-      // state.partner = partnerParser(result[2]);
-      // res.locals.state = state;
+      // console.log()
+      const state = convertToState(result[0]);
+      state.days = daysParser(result[1]);
+      res.locals.state = state;
       next();
     })
-    .catch((err) => {
-      res.locals.state = err;
+    .catch(err => res.send(err));
+};
+
+dbController.insertGoals = (req, res, next) => {
+  const goals = req.body;
+  console.log('goals: ', goals);
+  insertGoalPromise(goals)
+    .then(() => {
       next();
-    });
+    })
+    .catch(err => res.send(err));
 };
 
 module.exports = dbController;
 
-const goalsParser = (goalsArr) => {
-  const goalsObj = {};
-  goalsArr.forEach(goal => goalsObj[goal.goal] = goal.value)
-  return goalsObj;
+const daysParser = (daysArr) => {
+  const days = {};
+  daysArr.forEach((dayObj) => {
+    const currentDate = days[dayObj.date];
+    if (currentDate) {
+      currentDate[dayObj.goal] = dayObj.value;
+    } else {
+      days[dayObj.date] = {};
+      days[dayObj.date][dayObj.goal] = dayObj.value;
+    };
+  });
+  return days;
 };
 
-const partnerParser = partnerObj => ({
-  partnerName: partnerObj.name,
-  ID: partnerObj.user_id,
-});
-
-// const daysParser = (daysArr) => {
-//   const days = {};
-//   daysArr.forEach((dayObj) => {
-//     if (days[dayObj])
-//   });
-
-// }
+function convertToState(array) {
+  return array.reduce((accum, curr) => {
+    accum.userName = curr.username;
+    accum.userID = curr.user_id;
+    if (!accum.hasOwnProperty('goals')) { accum.goals = {}; }
+    accum.goals[curr.goal] = curr.value;
+    if (!accum.partner) {
+      accum.partner = {
+        partnerName: curr.partnername,
+        ID: curr.partnerid,
+        phone: curr.partnerphone,
+      }
+    }
+    return accum;
+  }, {});
+}
--- server/server.js
+++ server/server.js
@@ -9,14 +9,16 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 
-// app.use(express.static(path.resolve(__dirname, '../dist/')));
+app.use(express.static(path.resolve(__dirname, '../dist/')));
 
 
 app.get('/userdata', dbContoller.getUserState, (req, res) => {
   res.json(res.locals.state);
 });
 
-app.use(express.static(path.resolve(__dirname, '../dist/')));
+app.post('/newUser', dbContoller.newUser, (req, res) => res.send());
+
+app.post('/insertGoals', dbContoller.insertGoals, (req, res) => res.send());
 
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}...`));
